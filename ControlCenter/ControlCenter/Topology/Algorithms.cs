6691f0a0765ad7191d17077b8e4c6f6d535e7249
Mikboch <mikolaj.bochinski@gmail.com>
2021-02-07 21:03:12 +0100

    Szczeliny - proba ogarniecia

--- ControlCenter/ControlCenter/Topology/Algorithms.cs
+++ ControlCenter/ControlCenter/Topology/Algorithms.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 namespace ControlCenter {
     class Algorithms {
 
-        public static List<Path> AllPaths(Node staring, Node ending) {
+        public static List<Path> AllPaths(Node starting, Node ending) {
 
-            if(staring == null || ending == null) {
+            if (starting == null || ending == null) {
                 GUIWindow.PrintLog("One or both host Nodes do not exist!");
                 return null;
             }
 
             Graph graph = new Graph(ConfigLoader.routers.Count + ConfigLoader.hosts.Count + 1);
 
-            foreach(Connection edge in ConfigLoader.myConnections.Values) {
+            foreach (Connection edge in ConfigLoader.connections.Values) {
                 Node n1 = edge.endPoints.Item1;
                 Node n2 = edge.endPoints.Item2;
                 if (!n1.working || !n2.working)
@@ -27,7 +27,7 @@ namespace ControlCenter {
             }
 
             //Console.WriteLine("Paths between " + initialRouter.GetRouterID() + " and " + finalRouter.GetRouterID());
-            graph.printAllPaths(staring.GetAlgorithmID(), ending.GetAlgorithmID());
+            graph.printAllPaths(starting.GetAlgorithmID(), ending.GetAlgorithmID());
             List<List<int>> allPathsNodes = graph.allPaths;
 
             List<Path> allPaths = new List<Path>();
@@ -36,9 +36,43 @@ namespace ControlCenter {
             }
 
             allPaths.Sort();
-  
+
             return allPaths;
         }
+
+
+        //public static List<Path> AllPaths(Node staring, Node ending) {
+
+        //    if(staring == null || ending == null) {
+        //        GUIWindow.PrintLog("One or both host Nodes do not exist!");
+        //        return null;
+        //    }
+
+        //    Graph graph = new Graph(ConfigLoader.routers.Count + ConfigLoader.hosts.Count + 1);
+
+        //    foreach(Connection edge in ConfigLoader.myConnections.Values) {
+        //        Node n1 = edge.endPoints.Item1;
+        //        Node n2 = edge.endPoints.Item2;
+        //        if (!n1.working || !n2.working)
+        //            continue;
+
+        //        graph.addEdge(n1.GetAlgorithmID(), n2.GetAlgorithmID());
+        //        graph.addEdge(n2.GetAlgorithmID(), n1.GetAlgorithmID());
+        //    }
+
+        //    //Console.WriteLine("Paths between " + initialRouter.GetRouterID() + " and " + finalRouter.GetRouterID());
+        //    graph.printAllPaths(staring.GetAlgorithmID(), ending.GetAlgorithmID());
+        //    List<List<int>> allPathsNodes = graph.allPaths;
+
+        //    List<Path> allPaths = new List<Path>();
+        //    foreach (List<int> path in allPathsNodes) {
+        //        allPaths.Add(new Path(path));
+        //    }
+
+        //    allPaths.Sort();
+
+        //    return allPaths;
+        //}
 
     }
 }
